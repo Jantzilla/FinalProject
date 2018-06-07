e2cb0cefe722480751d89ab3b28ac9a99c743ca3
Jantz <jantzillac.93@gmail.com>
2018-06-07 16:45:50 -0500

    Add EndpointsAsyncTask() inside MainActivity.java

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,45 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+class EndpointsAsyncTask extends android.os.AsyncTask<android.util.Pair<android.content.Context, String>, Void, String> {
+    private static com.udacity.gradle.builditbigger.backend.myApi.MyApi myApiService = null;
+    private android.content.Context context;
+
+    @Override
+    protected String doInBackground(android.util.Pair<android.content.Context, String>... params) {
+        if(myApiService == null) {  // Only do this once
+            com.udacity.gradle.builditbigger.backend.myApi.MyApi.Builder builder = new com.udacity.gradle.builditbigger.backend.myApi.MyApi.Builder(com.google.api.client.extensions.android.http.AndroidHttp.newCompatibleTransport(),
+                    new com.google.api.client.extensions.android.json.AndroidJsonFactory(), null)
+                // options for running against local devappserver
+                // - 10.0.2.2 is localhost's IP address in Android emulator
+                // - turn off compression when running against local devappserver
+                .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                .setGoogleClientRequestInitializer(new com.google.api.client.googleapis.services.GoogleClientRequestInitializer() {
+                    @Override
+                    public void initialize(com.google.api.client.googleapis.services.AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws java.io.IOException {
+                        abstractGoogleClientRequest.setDisableGZipContent(true);
+                    }
+                });
+                // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (java.io.IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+    }
+}
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new EndpointsAsyncTask().execute(new android.util.Pair<android.content.Context, String>(this, "Manfred"));
     }
 
 
