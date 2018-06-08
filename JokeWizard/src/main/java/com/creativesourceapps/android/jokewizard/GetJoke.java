7527df643977155647e87edc44390566f00c5b93
Jantz <jantzillac.93@gmail.com>
2018-06-08 00:18:34 -0500

    Add more Android and Java library files to Git/Github

---
+++ JokeWizard/src/main/java/com/creativesourceapps/android/jokewizard/GetJoke.java
@@ -0,0 +1,9 @@
+package com.creativesourceapps.android.jokewizard;
+
+public class GetJoke {
+
+    public String getJoke() {
+
+                return "Why did the chicken cross the road? \n \n A hot chick!";
+    }
+}
