373ceb9af07ee2a8dacf966cb59f7f8b09360946
Jantz <jantzillac.93@gmail.com>
2018-06-09 05:47:34 -0500

    Changed localhost IP back to 10.0.2.2 from Genymotion's 10.0.3.2

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -24,7 +24,7 @@ class EndpointsAsyncTask extends android.os.AsyncTask<android.util.Pair<android.
                 // options for running against local devappserver
                 // - 10.0.2.2 is localhost's IP address in Android emulator
                 // - turn off compression when running against local devappserver
-                .setRootUrl("http://10.0.3.2:8080/_ah/api/")
+                .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                 .setGoogleClientRequestInitializer(new com.google.api.client.googleapis.services.GoogleClientRequestInitializer() {
                     @Override
                     public void initialize(com.google.api.client.googleapis.services.AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws java.io.IOException {
