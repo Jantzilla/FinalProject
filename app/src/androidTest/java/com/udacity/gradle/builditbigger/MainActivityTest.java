03c4c5fe2bd286eae10d50f22d1b68c4dd5d55e1
Jantz <jantzillac.93@gmail.com>
2018-06-09 04:04:29 -0500

    Add TextView Test after button click inside MainActivityTest.java

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -1,4 +1,37 @@
 package com.udacity.gradle.builditbigger;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void clickButton() {
+        onView(withId(R.id.tell_joke)).perform(click());
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.text_view_three))
+                .check(matches(withText("Why did the chicken cross the road? \n \n A hot chick!")));
+    }
+
 }
